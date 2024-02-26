27cb597c51daff3b79a6c00cca9e043298f878fe
Dawood Alkawaz <dalkawaz@gmail.com>
2024-02-26 22:05:00 +0300

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+// Remove main
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Dawood is the champion';
